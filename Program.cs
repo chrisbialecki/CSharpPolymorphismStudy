231a464361267f6a1fd75e99c0fbb493f66c13a5
chrisbialecki <chrisbialecki@yahoo.co.uk>
2016-12-19 23:14:46 +0000

    go

--- Program.cs
+++ Program.cs
@@ -6,11 +6,67 @@ using System.Threading.Tasks;
 
 namespace PolymorphismStudy
 {
+
+    public class Calculation
+    {
+        //polymorphism - allowed when the name stays the same but parameters change.
+        // just changing return type is not enough
+        
+        // signature is: name + type, kind and number of parameters
+        // return type is not part of signature
+
+
+
+        public int SomeMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        public string SomeMethod(int x, int y)
+        {
+            return "Hi there";
+        }
+        
+        
+       
+        
+        
+        
+        public int Add(int a, int b)
+        {
+            return a + b;               
+        }
+
+        public string Add(int a, int b, int c)
+        {
+            string message = "";
+            
+            if ((a + b + c) > 3)
+            {
+                message = "Total is more than 3";
+            }
+
+            return message;
+        }
+
+
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            // kuku na muniu
+ 
+
+            Calculation calc = new Calculation();
+            Console.WriteLine(calc.Add(1, 4).ToString());
+            Console.ReadKey();
+
+            Console.WriteLine(calc.Add(2, 3, 4));
+            Console.ReadKey();
+
+
+           
         }
     }
 }
